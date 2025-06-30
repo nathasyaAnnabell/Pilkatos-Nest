bce15b344f969090fe80995a666baf5bd91758cf
nathasyaAnnabell <loveyou3000ths@gmail.com>
2025-06-30 14:10:19 +0700

    refactor: deleted file testing and unused files

--- src/verify-token/verify-token.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { VerifyTokenMiddleware } from './verify-token.middleware';
-
-describe('VerifyTokenMiddleware', () => {
-  it('should be defined', () => {
-    expect(new VerifyTokenMiddleware()).toBeDefined();
-  });
-});
